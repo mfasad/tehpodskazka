7023f6471c6b7b67faa224805a18082817c55a1b
mfasad <mfasadads@gmail.com>
2026-04-08 22:47:53 +0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -193,4 +193,4 @@ document.addEventListener('DOMContentLoaded', function() {
     for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
     k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
 })(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=108292530','ym');
-ym(108292530,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:"dataLayer",referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});
+ym(108292530,'init',{clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});
